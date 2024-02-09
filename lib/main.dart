61ba33c52f61fc4c0cd1f2175e82a549689b494b
Vadym Pinchuk <vadym@labrys.tech>
2024-02-09 16:35:53 +0000

    Bad code

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,13 @@ void main() => runApp(
 
 class TodoApp extends StatefulWidget {
   @override
-  _TodoAppState createState() => _TodoAppState();
+  TodoAppState createState() => TodoAppState();
 }
 
-class _TodoAppState extends State<TodoApp> {
-  final List<String> _todoList = [];
-  final List<bool> _todoCheckList = [];
-  final TextEditingController _textFieldController = TextEditingController();
+class TodoAppState extends State<TodoApp> {
+  final List<String> mTL = [];
+  final List<bool> mTCL = [];
+  final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,64 +29,99 @@ class _TodoAppState extends State<TodoApp> {
       appBar: AppBar(title: Text('Simple TODO List')),
       body: ListView.builder(
         itemBuilder: (context, index) {
-          if (index < _todoList.length) {
-            return ListTile(
-              title: Text(
-                _todoList[index],
-                style: TextStyle(
-                  decoration: _todoCheckList[index] ? TextDecoration.lineThrough : null,
-                ),
-              ),
-              leading: Checkbox(
-                value: _todoCheckList[index],
-                onChanged: (bool? newValue) {
-                  setState(() {
-                    _todoCheckList[index] = newValue!;
-                  });
-                },
-              ),
+          if (index < mTL.length) {
+            return Dismissible(
+              key: Key(mTL[index]),
+              background: Container(
+                  color: Colors.red, child: Row(children: [Spacer(), Icon(Icons.delete)])),
+              child: ListTile(
+                  title: Text(mTL[index],
+                      style: TextStyle(
+                        decoration: mTCL[index] ? TextDecoration.lineThrough : null,
+                      )),
+                  leading: Checkbox(
+                      value: mTCL[index],
+                      onChanged: (bool? newValue) {
+                        setState(() {
+                          mTCL[index] = newValue!;
+                        });
+                      }),
+                  onTap: () {
+                    controller.text = mTL[index]; // Pre-fill the text field with the current task
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                              title: Text('Edit TODO'),
+                              content: TextField(
+                                controller: controller,
+                                decoration: InputDecoration(hintText: 'Edit your task...'),
+                              ),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: Text('CANCEL'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                    controller.clear();
+                                  },
+                                ),
+                                TextButton(
+                                    child: Text('UPDATE'),
+                                    onPressed: () {
+                                      if (controller.text.isNotEmpty) {
+                                        setState(() {
+                                          mTL[index] = controller.text;
+                                        });
+                                        controller.clear();
+                                      }
+                                      Navigator.of(context).pop();
+                                    })
+                              ]);
+                        });
+                  }),
             );
           }
           return null;
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('New TODO'),
-              content: TextField(
-                controller: _textFieldController,
-                decoration: InputDecoration(hintText: 'Enter something to do...'),
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: Text('CANCEL'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    _textFieldController.clear();
-                  },
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('New TODO'),
+                content: TextField(
+                  controller: controller,
+                  decoration: InputDecoration(hintText: 'Enter something to do...'),
                 ),
-                TextButton(
-                  child: Text('ADD'),
-                  onPressed: () {
-                    final task = _textFieldController.text;
-                    if (task.isNotEmpty) {
-                      setState(() {
-                        _todoList.add(task);
-                        _todoCheckList.add(false);
-                      });
-                      _textFieldController.clear();
-                    }
-                    ;
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
-            );
-          },
-        ),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('CANCEL'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      controller.clear();
+                    },
+                  ),
+                  TextButton(
+                    child: Text('ADD'),
+                    onPressed: () {
+                      final task = controller.text;
+                      if (task.isNotEmpty) {
+                        setState(() {
+                          mTL.add(task);
+                          mTCL.add(false);
+                        });
+                        controller.clear();
+                      }
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
         tooltip: 'Add Task',
         child: Icon(Icons.add),
       ),
