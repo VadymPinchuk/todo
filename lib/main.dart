8b991a39ee584e359ea436e79703ae7e7336e7a6
Vadym Pinchuk <vadym@labrys.tech>
2024-02-10 09:57:31 +0000

    Some stuff

--- lib/main.dart
+++ lib/main.dart
@@ -32,53 +32,52 @@ class TodoAppState extends State<TodoApp> {
           if (index < mTL.length) {
             return Dismissible(
               key: Key(mTL[index]),
+              onDismissed: (direction) {
+                // TODO: it
+              },
               background: Container(
                   color: Colors.red, child: Row(children: [Spacer(), Icon(Icons.delete)])),
-              child: ListTile(
-                  title: Text(mTL[index],
-                      style: TextStyle(
-                        decoration: mTCL[index] ? TextDecoration.lineThrough : null,
-                      )),
-                  leading: Checkbox(
-                      value: mTCL[index],
-                      onChanged: (bool? newValue) {
-                        setState(() {
-                          mTCL[index] = newValue!;
-                        });
-                      }),
-                  onTap: () {
-                    controller.text = mTL[index]; // Pre-fill the text field with the current task
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                              title: Text('Edit TODO'),
-                              content: TextField(
-                                controller: controller,
-                                decoration: InputDecoration(hintText: 'Edit your task...'),
+              child: TodoItemWidget(
+                title: mTL[index],
+                isCompleted: mTCL[index],
+                onTapped: () {
+                  controller.text = mTL[index]; // Pre-fill the text field with the current task
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                            title: Text('Edit TODO'),
+                            content: TextField(
+                              controller: controller,
+                              decoration: InputDecoration(hintText: 'Edit your task...'),
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text('CANCEL'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  controller.clear();
+                                },
                               ),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: Text('CANCEL'),
+                              TextButton(
+                                  child: Text('UPDATE'),
                                   onPressed: () {
+                                    if (controller.text.isNotEmpty) {
+                                      setState(() {
+                                        mTL[index] = controller.text;
+                                      });
+                                      controller.clear();
+                                    }
                                     Navigator.of(context).pop();
-                                    controller.clear();
-                                  },
-                                ),
-                                TextButton(
-                                    child: Text('UPDATE'),
-                                    onPressed: () {
-                                      if (controller.text.isNotEmpty) {
-                                        setState(() {
-                                          mTL[index] = controller.text;
-                                        });
-                                        controller.clear();
-                                      }
-                                      Navigator.of(context).pop();
-                                    })
-                              ]);
-                        });
-                  }),
+                                  })
+                            ]);
+                      });
+                },
+                onCompletedChanged: (value) {
+                  mTCL[index] = value;
+                  setState(() {});
+                },
+              ),
             );
           }
           return null;
@@ -124,6 +123,53 @@ class TodoAppState extends State<TodoApp> {
         },
         tooltip: 'Add Task',
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoItemWidget extends StatelessWidget {
+  final String title;
+  final bool isCompleted;
+  final VoidCallback? onTapped;
+  final ValueChanged<bool> onCompletedChanged;
+
+  const TodoItemWidget({
+    Key? key,
+    required this.title,
+    required this.isCompleted,
+    required this.onTapped,
+    required this.onCompletedChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTapped,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  decoration: isCompleted ? TextDecoration.lineThrough : null,
+                ),
+              ),
+            ),
+            Checkbox(
+              value: isCompleted,
+              onChanged: (value) {
+                if (value == null)
+                  return;
+                else
+                  onCompletedChanged(value);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
